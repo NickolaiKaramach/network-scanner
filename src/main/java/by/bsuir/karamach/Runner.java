40d64386d072a15969f1d6141379416a3079c30d
NickolaiKaramach <nick.karamach@gmail.com>
2019-02-15 19:23:00 +0300

    Pre show additions

--- src/main/java/by/bsuir/karamach/Runner.java
+++ src/main/java/by/bsuir/karamach/Runner.java
@@ -2,28 +2,74 @@ package by.bsuir.karamach;
 
 import by.bsuir.karamach.finder.IpLocalFinder;
 import by.bsuir.karamach.process.ProcessRunner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.util.List;
 
 public class Runner {
 
     private static final String ARP_CONSOLE_COMMAND = "arp ";
+    private static final Logger logger = LogManager.getLogger();
+    private static final String subnet = "192.168.33";
+    private static final String HEX_FORMAT = "%02X%s";
+    private static final String EMPTY_STRING = "";
+    private static final String MINUS = "-";
+    private static final String DEVICES_SPLITTER = "\n**********************************************************";
+    private static final String TOOLTIP = "host_name (ip_address) at MAC_address on network_interface";
 
     public static void main(String[] args) throws Exception {
 
-        String subnet = "192.168.33";
+        InetAddress localHost = InetAddress.getLocalHost();
+        byte[] macBytes = NetworkInterface.getByInetAddress(localHost).getHardwareAddress();
+
+        String hostName = localHost.getHostName();
+        String macAddress = getFormatMacAddress(macBytes).toString();
+
+        StringBuilder result = new StringBuilder();
+
+        result.append("My computer:")
+                .append("\nHost name: ")
+                .append(hostName)
+                .append("\nMac: ")
+                .append(macAddress)
+                .append("\n")
+                .append("Others:");
+
 
         IpLocalFinder hostFinder = new IpLocalFinder();
         List<String> hostNames = hostFinder.getAllIps(subnet);
 
-        System.out.println(hostNames);
+        StringBuilder otherDevicesInfo = getNetworkInfo(hostNames);
+        result.append(otherDevicesInfo);
 
+        System.out.println(result);
 
+    }
+
+    private static StringBuilder getNetworkInfo(List<String> hostNames) throws IOException {
+        StringBuilder otherDevicesInfo = new StringBuilder();
         for (String currentName : hostNames) {
-            System.out.println("******************************************************************************");
-            System.out.println(ProcessRunner.executeCommand(ARP_CONSOLE_COMMAND + currentName));
-            System.out.println("******************************************************************************");
+            otherDevicesInfo.append(DEVICES_SPLITTER)
+                    .append("\n")
+                    .append(TOOLTIP)
+                    .append("\n\n")
+                    .append(ProcessRunner.executeCommand(ARP_CONSOLE_COMMAND + currentName));
         }
+        return otherDevicesInfo;
+    }
+
+    private static StringBuilder getFormatMacAddress(byte[] macBytes) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < macBytes.length; i++) {
+            sb.append(String.format(HEX_FORMAT, macBytes[i], (i < macBytes.length - 1) ? MINUS : EMPTY_STRING));
+        }
+
+        return sb;
     }
 
 
@@ -35,15 +81,15 @@ public class Runner {
 //                NetworkInterface networkInterface = (NetworkInterface) networkInterfaces.nextElement();
 //                Enumeration inetAddresses = networkInterface.getInetAddresses();
 //
-//                System.out.println("***********************************************");
-//                System.out.println("Host name: " + networkInterface.getDisplayName());
+//                logger.info("***********************************************");
+//                logger.info("Host name: " + networkInterface.getDisplayName());
 //
 //                while (inetAddresses.hasMoreElements()) {
 //
 //                    InetAddress inetAddress = (InetAddress) inetAddresses.nextElement();
 //
-//                    System.out.println("Host address: " + inetAddress.getHostAddress());
-//                    System.out.println(inetAddress.getHostName());
+//                    logger.info("Host address: " + inetAddress.getHostAddress());
+//                    logger.info(inetAddress.getHostName());
 //
 //                }
 //
@@ -55,15 +101,15 @@ public class Runner {
 //                        sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
 //                    }
 //
-//                    System.out.println("Mac: " + sb.toString());
+//                    logger.info("Mac: " + sb.toString());
 //                }
 //
-//                System.out.println("***********************************************\n");
+//                logger.info("***********************************************\n");
 //
 //
 //            }
 //        } catch (SocketException ex) {
-//            System.out.println(ex.getMessage());
+//            logger.info(ex.getMessage());
 //        }
 //
 //    }
